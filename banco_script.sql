79f5cc5979cb21adcbb15ea4eb5fb439ce6e6ba3
Fellipe Freire <45107633+FellipeF@users.noreply.github.com>
2023-02-10 15:12:03 -0300

    Mudança no script original da criação do Banco
    Feita uma PK composta em aluguel_livro

--- banco_script.sql
+++ banco_script.sql
@@ -4,42 +4,43 @@ use biblioteca;
 
 create table livro(
 	id int primary key auto_increment,
-    nome varchar(40) not null,
-    isbn varchar (25) not null,
-    autor varchar(30) not null,
-    genero varchar(20) not null,
-    paginas smallint not null,
-    edicao tinyint not null,
-    quantidade tinyint not null,
-    secao tinyint not null,
-    contemimagem bool
+    	nome varchar(40) not null,
+    	isbn varchar (25) not null,
+    	autor varchar(30) not null,
+    	genero varchar(20) not null,
+    	paginas smallint not null,
+    	edicao tinyint not null,
+    	quantidade tinyint not null,
+    	secao tinyint not null,
+    	contemimagem bool
 );
 
 create table estudante(
 	id int primary key auto_increment,
 	nome varchar(50) not null,
-    email varchar(25) not null,
-    telefone varchar(20) not null,
-    matricula varchar(20) not null,
-    cpf varchar(20) not null,
-    codigo_acesso varchar(15) not null,
-    atrasado bool
+    	email varchar(25) not null,
+    	telefone varchar(20) not null,
+    	matricula varchar(20) not null,
+    	cpf varchar(20) not null,
+   	codigo_acesso varchar(15) not null,
+    	atrasado bool
 );
     
 create table funcionario(
-   id int primary key auto_increment,
-   nome varchar(50) not null,
-   cpf varchar(20) not null,
-   telefone varchar(20) not null,
-   email varchar(20) not null,
-   codigo_acesso varchar(15) not null
+   	id int primary key auto_increment,
+   	nome varchar(50) not null,
+   	cpf varchar(20) not null,
+   	telefone varchar(20) not null,
+   	email varchar(20) not null,
+   	codigo_acesso varchar(15) not null
 );
 
 create table aluguel_livro(
-	id int primary key auto_increment,
-    foreign key(id) references estudante(id),
-    isbn varchar(25) not null,
-    nome_livro varchar(40) not null,
-    nome_estudante varchar(50) not null,
-    data_aluguel timestamp default current_timestamp
+	id_alugel int auto_increment,
+	id_livro int,
+	id_estudante int,
+	PRIMARY KEY (id_aluguel, id_livro, id_estudante) 
+	foreign key(id_livro) references livro(id),
+	foreign key(id_estudante) references estudante(id),
+    	data_aluguel timestamp default current_timestamp
 );
